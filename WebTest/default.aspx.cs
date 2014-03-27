d3c1ef77d8c79dcb21388bf15ef9a950c3241a99
Jeonghee Lee <heejuk@hotmail.com>
2014-03-26 21:23:24 -0400

    upload source files again

---
+++ WebTest/default.aspx.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace _03252014
+{
+    public partial class _default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            Label1.Text = DateTime.Now.Date.ToShortDateString();
+        }
+
+
+    }
+}
